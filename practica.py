57b4815e5ecbbe154dd8ecc717decf2c56679b78
Sam <sam.brumwell2022@gmail.com>
2025-05-17 18:29:03 +0200

    algunes coses de mes

--- practica.py
+++ practica.py
@@ -1,22 +1,42 @@
 import numpy as np
+"""
+Tot el programa serà bàsicament tota una classe ( per ara, si no va be doncs potser caldràn funcions fora de la classe, ns), 
+ja veurem com farem la lectura de dades, pero per ara hem de programar l'algoritme en sí
+
+
+
+
+
+"""
 class CKY:
     def __init__(self, paraula):
         self.normes = {}
-        self.n = len(paraula)
+        #self.n = len(paraula)
         pass
     def crear_taula(self,n):
-        self.taula = np.zeros((n,n))
+        taula = [[[] for i in range(n-j)]for j in range(n)]
         
-        return(self.taula)
+
+     
+        return(taula)
     def afegir_norma(self,index,norma):
-        self.normes[index] = [norma]
+        self.normes[index] = norma
     def resol(self, paraula):
         taula = self.crear_taula(self.n)
-        for i in range(0,len(paraula)):
-            taula[0][i] = []
+        l = len(paraula)
+        taula = self.nivell1(taula,paraula)
+        for i in range(0,l):
+            for j in range(0, l-i):
+                taula[0][i] = []
             for valor in self.normes.keys():
-                if self.normes[valor] == paraula[i]:
+                if paraula[i] in self.normes[valor]:
                     taula[0][i] += [valor]
+    def nivell1(self,taula,paraula):
+        for i in range(0,len(paraula)):
+            for norma in self.normes:
+                if paraula[i] in self.normes[norma]:
+                    taula[0][i].append(norma)
+        return taula
 
         pass
 
